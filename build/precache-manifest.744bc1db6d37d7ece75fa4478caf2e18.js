4f8d4a8737b07a017dd0ef4f754536b53cc652db
Вадим Змиевский <vadimzmievskij@MacBook-Pro-Vadim.local>
2020-02-12 15:31:02 +0200

    build

---
+++ build/precache-manifest.744bc1db6d37d7ece75fa4478caf2e18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d4d7c1ede53eb8fd51069dacfee7c9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a0b5870d3b85abd7a16",
+    "url": "/static/css/main.3098c66b.chunk.css"
+  },
+  {
+    "revision": "b9c1fd1c5d25777c0baa",
+    "url": "/static/js/2.ad309e3c.chunk.js"
+  },
+  {
+    "revision": "7f65314857d18c82d233d14129fe797a",
+    "url": "/static/js/2.ad309e3c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a0b5870d3b85abd7a16",
+    "url": "/static/js/main.18aee080.chunk.js"
+  },
+  {
+    "revision": "312b023ebd59b2952bae",
+    "url": "/static/js/runtime-main.91a58624.js"
+  }
+]);
